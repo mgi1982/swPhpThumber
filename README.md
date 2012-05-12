3a8a8c5096a711fd897336fd50a39562b76d5de9
Marcos Ibañez <mgi1982@gmail.com>
2012-05-12 00:34:05 -0300

    First functional version. Does not cache the images yet

--- README.md
+++ README.md
@@ -2,3 +2,36 @@ swPhpThumber
 ============
 
 A very simple script to automate and distribute thumbmail generator writtern in PHP.
+
+The idea is to have the ability to detach the thumbnail generation process
+from existing webapplications by providing a very simple Rest API.
+
+The following are some examples that will work:
+
+http://yoursite.com/images/someimage.jpg?w=200&h=100
+
+This will return a 200x100 version of the someimage.jpg file. No scaling will
+be perforemed on someimage.jpg.
+
+http://yoursite.com/images/someimage.jpg?w=200
+
+This will return a rescales version of someimage.jpg with a maximum width of 200x
+
+http://yoursite.com/images/someimage.jpg?h=100
+
+Same as before but the returned image will have 100px of height.
+
+http://yoursite.com/images/someimage.jpg?w=200&scale=no
+
+This will return a 200xOriginalHeight version of the someimage.jpg file, no
+scaling will be applied.
+
+
+Some features that will be added in the future:
+1. Support of Gearman, further improving your ability to detach and enqueue
+your thumb generation process.
+2. Caching configuration via url/local files, this will allow you to indicate
+wether you want to force the thumb regeneration or if you are ok with getting
+a new version of the image.
+3. Inline image generation to embed the image directly into css/html files
+reducing the ammount of requests.
--- src/swPhpThumber.php
+++ src/swPhpThumber.php
@@ -1,5 +1,100 @@
 <?php
-die(var_dump($_GET));
 $im = file_get_contents($_REQUEST['img']); 
+if(!file_exists($_REQUEST['img'])) {
+    header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+}
+
+class SimpleImage {
+ 
+   var $image;
+   var $image_type;
+ 
+   function load($filename) {
+ 
+      $image_info = getimagesize($filename);
+      $this->image_type = $image_info[2];
+      if( $this->image_type == IMAGETYPE_JPEG ) {
+ 
+         $this->image = imagecreatefromjpeg($filename);
+      } elseif( $this->image_type == IMAGETYPE_GIF ) {
+ 
+         $this->image = imagecreatefromgif($filename);
+      } elseif( $this->image_type == IMAGETYPE_PNG ) {
+ 
+         $this->image = imagecreatefrompng($filename);
+      }
+   }
+   function save($filename, $image_type=IMAGETYPE_JPEG, $compression=75, $permissions=null) {
+ 
+      if( $image_type == IMAGETYPE_JPEG ) {
+         imagejpeg($this->image,$filename,$compression);
+      } elseif( $image_type == IMAGETYPE_GIF ) {
+ 
+         imagegif($this->image,$filename);
+      } elseif( $image_type == IMAGETYPE_PNG ) {
+ 
+         imagepng($this->image,$filename);
+      }
+      if( $permissions != null) {
+ 
+         chmod($filename,$permissions);
+      }
+   }
+   function output($image_type=IMAGETYPE_JPEG) {
+ 
+      if( $image_type == IMAGETYPE_JPEG ) {
+         imagejpeg($this->image);
+      } elseif( $image_type == IMAGETYPE_GIF ) {
+ 
+         imagegif($this->image);
+      } elseif( $image_type == IMAGETYPE_PNG ) {
+ 
+         imagepng($this->image);
+      }
+   }
+   function getWidth() {
+ 
+      return imagesx($this->image);
+   }
+   function getHeight() {
+ 
+      return imagesy($this->image);
+   }
+   function resizeToHeight($height) {
+ 
+      $ratio = $height / $this->getHeight();
+      $width = $this->getWidth() * $ratio;
+      $this->resize($width,$height);
+   }
+ 
+   function resizeToWidth($width) {
+      $ratio = $width / $this->getWidth();
+      $height = $this->getheight() * $ratio;
+      $this->resize($width,$height);
+   }
+ 
+   function scale($scale) {
+      $width = $this->getWidth() * $scale/100;
+      $height = $this->getheight() * $scale/100;
+      $this->resize($width,$height);
+   }
+ 
+   function resize($width,$height) {
+      $new_image = imagecreatetruecolor($width, $height);
+      imagecopyresampled($new_image, $this->image, 0, 0, 0, 0, $width, $height, $this->getWidth(), $this->getHeight());
+      $this->image = $new_image;
+   }      
+ 
+}
+
 header('Content-type: ' . image_type_to_mime_type(exif_imagetype($_REQUEST['img']))); 
-echo $im; 
+$img = new SimpleImage();
+$img->load($_REQUEST['img']);
+if(isset($_REQUEST['w']) && isset($_REQUEST['h'])) {
+    $img->resize($_REQUEST['w'], $_REQUEST['h']);
+} elseif(isset($_REQUEST['w'])) {
+    $img->resizeToWidth($_REQUEST['w']);
+} elseif(isset($_REQUEST['h'])) {
+    $img->resizeToHeight($_REQUEST['h']);
+}
+echo $img->output();
